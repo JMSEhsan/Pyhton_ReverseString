fdc403c75cd663f6225e571b844828c138e0783a
Ehsan <JMSEhsan@gmail.com>
2021-12-10 23:55:43 -0700

    Python_SelfPractice_ReverseString

---
+++ Python_ReverseString.py
@@ -0,0 +1,7 @@
+# Reverse a String
+#https://www.w3schools.com/python/trypython.asp?filename=demo_howto_reverse_string
+
+txt1 = "Hello World"[::-1]
+txt2 = "Reverse a String"[::-1]
+print(txt1)
+print(txt2)
